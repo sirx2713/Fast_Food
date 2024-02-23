a18d21a37bdadfa08daa2feb4cb9861976955967
Sirx <bradx2713@gmail.com>
2024-02-23 20:54:29 +0200

    Initial Commit

---
+++ Fast_Food/Fast_FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fast_FoodApp.swift
+//  Fast_Food
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Fast_FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
